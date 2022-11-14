6234f73a4e8cecfef6fec177f93bb330a8850928
Pedro Marques <ppmarq1@gmail.com>
2022-11-14 14:15:13 +0000

    setup JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
-  root to: "home#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+  namespace :api do
+    resources :users, only:  [:index, :show] do
+    end
+  end
