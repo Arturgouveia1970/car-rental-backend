3c9f46cf42fb4bf48f2afc963ad7349385a1fe41
Artur Gouveia <artgouveiag@gmail.com>
2023-03-07 15:45:40 +0000

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,5 @@ fifth_car = Car.create(model: 'Nissan GT-R', year: '2015', price: 2550, image: '
 sixth_car = Car.create(model: 'Ford GT', year: '2016', price: 1550, image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Nxrx266lwb12Snk19ZTazAHaEK%26pid%3DApi&f=1&ipt=9eb90592a525f00f96d10ab9ec0ed0c76c7d111abcedf5a0e31ce604254742ab&ipo=images')
 seventh_car = Car.create(model: 'Porche 911', year: '2014', price: 2200, image: 'https://www.pixelstalk.net/wp-content/uploads/2016/09/HD-Porsche-911-Wallpaper.jpeg')
 
-# Car.destroy_all
-# Car.create
+Car.destroy_all
+Car.create
