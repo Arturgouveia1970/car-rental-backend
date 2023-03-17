f092be22b6943828cb7790b4fb61cbd080faf9d0
Artur Gouveia <artgouveiag@gmail.com>
2023-03-17 08:37:11 +0000

    deploying

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 bundle install
-# bundle exec rake assets:precompile
-# bundle exec rake assets:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
