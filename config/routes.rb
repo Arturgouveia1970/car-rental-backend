9536fc9dd73d49badf25ff37d041ca9044e02be8
Artur Gouveia <artgouveiag@gmail.com>
2023-04-03 17:39:17 +0100

    changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,33 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
 
-  get '/', to: 'user#login'
-  get 'login/:name', to: 'users#login'
-  get 'api/v1/users/car/:id', to: 'users#user_car'
-  post 'register', to: 'users#register'
+  # get '/', to: 'user#login'
+  # get 'login/:name', to: 'users#login'
+  # get 'api/v1/users/car/:id', to: 'users#user_car'
+  # post 'register', to: 'users#register'
 
-  namespace :api do
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :cars
+  #     resources :reservations
+  #   end
+  # end
+
+  root 'api/v1/users#index'
+  post 'api/v1/auth/login', to: 'authentication#login'
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :cars
-      resources :reservations
+      resources :users, only: %i[index show create update destroy] do
+        resources :reservations, only: %i[index show create]
+      end
+      resources :cars, only: %i[index show create destroy] do
+        resources :reservations, only: %i[index show create]
+      end
     end
   end
 end
