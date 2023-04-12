f7d8734e4185ca5d19a5fe8846ca80a2e1096784
Artur Gouveia <artgouveiag@gmail.com>
2023-04-12 23:28:18 +0100

    changed users controller again

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -54,6 +54,29 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def register
+    if User.find_by(email: params[:email].downcase)
+      render json: { error: 'Useremail already exists! please choose another one.' }, status: :not_acceptable
+    else
+      @user = User.new(user_params)
+      @user.email = @user.email.downcase
+      if @user.save
+        render json: { user: @user, logged_in: true }, status: :created
+      else
+        render json: { error: 'There was an error, please try again!' }, status: :internal_server_error
+      end
+    end
+  end
+
+  def login
+    @user = User.find_by(email: params[:email].downcase)
+    if @user
+      render json: { user: @user, logged_in: true }, status: :ok
+    else
+      render json: { error: 'email is invalid.' }, status: :not_acceptable
+    end
+  end
+
   private
 
   def find_user
