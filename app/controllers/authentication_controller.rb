af7a0b0b26624d99623895ab8688c55b6e00c9d5
Artur Gouveia <artgouveiag@gmail.com>
2023-04-18 11:39:10 +0100

    authentication methods

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -9,7 +9,7 @@ class AuthenticationController < ApplicationController
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
       render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
-                     name: @user.name, role: @user.role, id: @user.id }, status: :ok
+                     name: @user.email, role: @user.role, id: @user.id }, status: :ok
     else
       render json: { error: 'Unauthorized' }, status: :unauthorized
     end
