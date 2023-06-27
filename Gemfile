ee2d01b0d209710045929897f7822899e4972ea8
Artur <artgouveiag@gmail.com>
2023-06-27 22:38:42 +0100

    devise upgrade

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '3.1.2'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.5.1'
 
-gem 'devise', '~> 4.9.0'
+gem 'devise', '~> 4.9', '>= 4.9.2'
 
 gem 'jwt'
 
--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require 'sprockets/railtie'
-require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
