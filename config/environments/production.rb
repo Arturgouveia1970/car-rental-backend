4abb0b90f67b2152e071eb9259dc338b0767c625
Artur <artgouveiag@gmail.com>
2023-07-19 21:51:36 +0100

    config secret key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "car_rental_production"
