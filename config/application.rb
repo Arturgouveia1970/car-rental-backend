85851823b888df32081a470e568eb48645a48839
Artur <artgouveiag@gmail.com>
2023-07-20 12:49:19 +0100

    secret key

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module CarRental
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
+    config.secret_key_base = ENV['SECRET_KEY_BASE']
   end
 end
