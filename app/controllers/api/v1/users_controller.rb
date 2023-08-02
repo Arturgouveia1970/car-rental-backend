422238673ef6f7de10e0c01391cb8a4bdca487d7
Artur <artgouveiag@gmail.com>
2023-08-02 15:43:39 +0100

    changed user controller again

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,57 +2,57 @@ class Api::V1::UsersController < ApplicationController
   # before_action :authorize_request, except: %i[index create]
   # before_action :find_user, except: %i[create index]
 
-  ALLOWED_DATA = %(name email password).freeze
+  # ALLOWED_DATA = %(name email password).freeze
 
-  # GET /users
-  def index
-    users = User.all
-    render json: users, status: :ok
-  end
+  # # GET /users
+  # def index
+  #   users = User.all
+  #   render json: users, status: :ok
+  # end
 
-  # GET /users/:id
-  def show
-    render json: @user, status: :ok
-  end
+  # # GET /users/:id
+  # def show
+  #   render json: @user, status: :ok
+  # end
 
-  # POST /users
-  def create
-    data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) && allow != 'role' }
-    return render json: { error: 'Empty body. Could not create user.' }, status: :unprocessable_entity if data.blank?
+  # # POST /users
+  # def create
+  #   data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) && allow != 'role' }
+  #   return render json: { error: 'Empty body. Could not create user.' }, status: :unprocessable_entity if data.blank?
 
-    user = User.new(data)
-    if user.save
-      render json: user, status: :ok
-    else
-      render json: { error: 'Could not create user.' }, status: :unprocessable_entity
-    end
-  end
+  #   user = User.new(data)
+  #   if user.save
+  #     render json: user, status: :ok
+  #   else
+  #     render json: { error: 'Could not create user.' }, status: :unprocessable_entity
+  #   end
+  # end
 
-  # PUT /users/:id
-  def update
-    if current_user.is? :admin
-      data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
-      return render json: { error: 'Empty body. Could not update user.' }, status: :unprocessable_entity if data.blank?
+  # # PUT /users/:id
+  # def update
+  #   if current_user.is? :admin
+  #     data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
+  #     return render json: { error: 'Empty body. Could not update user.' }, status: :unprocessable_entity if data.blank?
 
-      if @user.update(data)
-        render json: @user, status: :ok
-      else
-        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-      end
-    else
-      render json: { error: 'Unauthorized.' }, status: :unauthorized
-    end
-  end
+  #     if @user.update(data)
+  #       render json: @user, status: :ok
+  #     else
+  #       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+  #     end
+  #   else
+  #     render json: { error: 'Unauthorized.' }, status: :unauthorized
+  #   end
+  # end
 
-  # DELETE /users/:id
-  def destroy
-    if current_user.is? :admin
-      @user.destroy
-      render json: @user, status: :ok
-    else
-      render json: { error: 'Unauthorized.' }, status: :unauthorized
-    end
-  end
+  # # DELETE /users/:id
+  # def destroy
+  #   if current_user.is? :admin
+  #     @user.destroy
+  #     render json: @user, status: :ok
+  #   else
+  #     render json: { error: 'Unauthorized.' }, status: :unauthorized
+  #   end
+  # end
 
   def register
     if User.find_by(email: params[:email].downcase)
