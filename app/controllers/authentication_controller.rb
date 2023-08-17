5263d0e361adb7eda8f73556ea2d2387ecd5d18c
Arturgouveia1970 <artgouveiag@gmail>
2023-08-17 11:03:02 +0100

    changed routes

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,19 +1,19 @@
 class AuthenticationController < ApplicationController
-  ALLOWED_DATA = %(email password).freeze
+  # ALLOWED_DATA = %(name email password).freeze
 
-  # POST /auth/login
-  def login
-    data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
-    @user = User.find_by_name!(data[:email])
-    if @user.valid_password?(data[:password])
-      token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token:, exp: time.strftime('%d-%m-%Y %H:%M'),
-                     name: @user.email, role: @user.role, id: @user.id }, status: :ok
-    else
-      render json: { error: 'Unauthorized' }, status: :unauthorized
-    end
-  rescue ActiveRecord::RecordNotFound
-    render json: { errors: 'User not found' }, status: :not_found
-  end
+  # # POST /auth/login
+  # def login
+  #   data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
+  #   @user = User.find_by_name!(data[:email])
+  #   if @user.valid_password?(data[:password])
+  #     token = JsonWebToken.encode(user_id: @user.id)
+  #     time = Time.now + 24.hours.to_i
+  #     render json: { token:, exp: time.strftime('%d-%m-%Y %H:%M'),
+  #                    name: @user.email, role: @user.role, id: @user.id }, status: :ok
+  #   else
+  #     render json: { error: 'Unauthorized' }, status: :unauthorized
+  #   end
+  # rescue ActiveRecord::RecordNotFound
+  #   render json: { errors: 'User not found' }, status: :not_found
+  # end
 end
