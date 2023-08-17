0cebb3c404eef10764a4538918012d0bbace49a8
Arturgouveia1970 <artgouveiag@gmail>
2023-08-17 11:57:16 +0100

    eliminated password

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true
-  validates :password, presence: true, length: { maximum: 20 }
+  # validates :password, presence: true, length: { maximum: 20 }
 end
