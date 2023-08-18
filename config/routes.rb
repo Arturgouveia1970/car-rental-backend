d1a96c5aac4d197b8077fe07d531ea87f015c8f4
Arturgouveia1970 <artgouveiag@gmail>
2023-08-18 15:44:43 +0100

    route changes

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 
 
   post 'api/v1/register/:name/:email', to: 'users#register'
-  get 'api/v1/users/login/:email', to: 'users#login'
+  get 'api/v1/login/:email', to: 'users#login'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[index show create update destroy register login ] do
