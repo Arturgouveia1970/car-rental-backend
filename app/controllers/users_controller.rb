10fcde29e8867baf09edde5fa954971604ffe576
Arturgouveia1970 <artgouveiag@gmail>
2023-08-18 22:28:18 +0100

    changed users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,9 +24,13 @@ class UsersController < ApplicationController
 
   private
 
-  def find_user
-    @user = User.find_by_id!(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render json: { errors: 'User not found' }, status: :not_found
+  # def find_user
+  #   @user = User.find_by_id!(params[:id])
+  # rescue ActiveRecord::RecordNotFound
+  #   render json: { errors: 'User not found' }, status: :not_found
+  # end
+
+  def user_params
+    params.permit(:name, :email)
   end
 end
