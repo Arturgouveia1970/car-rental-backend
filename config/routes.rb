c4c73a3aecdb6e247dbe475a72b9ca8efafc13cb
Arturgouveia1970 <artgouveiag@gmail>
2023-08-18 20:01:48 +0100

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,14 +23,28 @@ Rails.application.routes.draw do
 
   post 'api/v1/register/:name/:email', to: 'users#register'
   get 'api/v1/login/:email', to: 'users#login'
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      resources :users, only: %i[index show create update destroy register login ] do
-        resources :reservations, only: %i[index show create]
-      end
-      resources :cars, only: %i[index show create destroy] do
-        resources :reservations, only: %i[index show create]
-      end
-    end
-  end
+  # namespace :api, defaults: { format: :json } do
+  #   namespace :v1 do
+  #     resources :users, only: %i[index show create update destroy register login ] do
+  #       resources :reservations, only: %i[index show create]
+  #     end
+  #     resources :cars, only: %i[index show create destroy] do
+  #       resources :reservations, only: %i[index show create]
+  #     end
+  #   end
+  # end
+
+  get 'api/v1/cars', to: 'cars#index'
+  get 'api/v1/car/:id', to: 'cars#show'
+  post 'api/v1/car', to: 'cars#create'
+  delete 'api/v1/car/:id', to: 'cars#delete'
+  get 'api/v1/reserve/cars/:date', to: 'cars#reserve'
+  get 'api/v1/cars/:user_id', to: 'cars#user_cars' 
+
+  # Reservation endpoints
+  post 'api/v1/reservation/:user_id/:car_id/:city/:date', to: 'reservations#create'
+  get 'api/v1/reservations/:user_id', to: 'reservations#index'
+
+  # Redirect to api-docs
+  get '*path', to: redirect('/api-docs')
 end
