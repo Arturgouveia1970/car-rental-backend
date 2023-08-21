32b17148c737bb8ee85bc5b1670ce179fc13036b
Arturgouveia1970 <artgouveiag@gmail>
2023-08-21 19:24:13 +0100

    more addictions to car routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,21 @@ Rails.application.routes.draw do
   # register and login paths
   post 'api/v1/register/:name/:email', to: 'users#register'
   get 'api/v1/users/login/:email', to: 'users#login'
+
   get 'api/v1/reserve/cars/:date', to: 'cars#reserve'
   get 'api/v1/cars/:user_id', to: 'cars#user_cars'
 
   # Reservation endpoints
   post 'api/v1/reservation/:user_id/:car_id/:city/:date', to: 'reservations#create'
-  get 'api/v1/reservations/:user_id', to: 'reservations#index'  
+  get 'api/v1/reservations/:user_id', to: 'reservations#index'
+  
+  # Car endpoints
+  get 'api/v1/cars', to: 'cars#index'
+  get 'api/v1/car/:id', to: 'cars#show'
+  post 'api/v1/car', to: 'cars#create'
+  delete 'api/v1/car/:id', to: 'cars#delete'
+  get 'api/v1/reserve/cars/:date', to: 'cars#reserve'
+  get 'api/v1/cars/:user_id', to: 'cars#user_cars'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
