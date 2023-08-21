591e54360aa586800ff6a39f6a802af3630216f8
Arturgouveia1970 <artgouveiag@gmail>
2023-08-21 19:46:29 +0100

    one more change

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,6 @@ Rails.application.routes.draw do
   # register and login paths
   post 'api/v1/register/:name/:email', to: 'users#register'
   get 'api/v1/users/login/:email', to: 'users#login'
-
-  get 'api/v1/reserve/cars/:date', to: 'cars#reserve'
-  get 'api/v1/cars/:user_id', to: 'cars#user_cars'
-
-  # Reservation endpoints
-  post 'api/v1/reservation/:user_id/:car_id/:city/:date', to: 'reservations#create'
-  get 'api/v1/reservations/:user_id', to: 'reservations#index'
   
   # Car endpoints
   get 'api/v1/cars', to: 'cars#index'
@@ -22,6 +15,10 @@ Rails.application.routes.draw do
   delete 'api/v1/car/:id', to: 'cars#delete'
   get 'api/v1/reserve/cars/:date', to: 'cars#reserve'
   get 'api/v1/cars/:user_id', to: 'cars#user_cars'
+
+  # Reservation endpoints
+  post 'api/v1/reservation/:user_id/:car_id/:city/:date', to: 'reservations#create'
+  get 'api/v1/reservations/:user_id', to: 'reservations#index'
 
   # namespace :api, defaults: { format: :json } do
   #   namespace :v1 do
