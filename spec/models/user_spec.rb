bd1d95af665fe59a9f8eb3386e4cf155e7cfc1cc
Arturgouveia1970 <artgouveiag@gmail>
2023-08-31 13:18:52 +0100

    changes to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { described_class.new(email: 'test@test.com', password: 'Eduardo1234', name: 'test', role: 'admin') }
+  subject { described_class.new(email: 'test@test.com', name: 'test') }
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
   it 'is not valid without an email' do
     subject.email = nil
     expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a password more than 6 characters' do
-    subject.password = 'a' * 5
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a password' do
-    subject.password = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is valid without a role (default role = user)' do
-    subject.role = nil
-    expect(subject).to be_valid
   end
 end
