51b4c6d88864571ecdca571155c9f34b654914d7
Artur <artgouveiag@gmail.com>
2023-09-06 19:37:20 +0100

    changed car deletecontroller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -18,7 +18,7 @@ class CarsController < ApplicationController
   end
 
   def delete
-    @car = Car.find(params[:id])
+    @car = Car.find(params[:user_id]).car
     if @car.destroy!
       render json: { success: 'The car has been deleted successfully' }, status: :ok
     else
